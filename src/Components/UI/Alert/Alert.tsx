c27d0b4b1e398896c09a6508ca03387e2cd4feb7
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-10-07 18:53:52 +0600

    В файле Alert.tsx был изменен размер и в файле ModalWindow.tsx был создан стейт для закрытия Alert компонента. Так же была добавлена кнопка для вывода модалки. Так же были изменения вывода компонента Alert. Задание #58 выполнена.

--- src/Components/UI/Alert/Alert.tsx
+++ src/Components/UI/Alert/Alert.tsx
@@ -7,23 +7,23 @@ interface IProps extends React.PropsWithChildren {
 
 const Alert: React.FC<IProps> = ({ type, children, onDismiss }) => {
   return (
-    <div className={`alert alert-${type}`} role="alert">
+    <div className={`alert alert-${type} w-50`} role="alert">
       <div className="d-flex justify-content-between">
         <div>{children}</div>
-          {onDismiss !== undefined ? (
-            <button
-              type="button"
-              className="border-0 bg-transparent"
-              onClick={onDismiss}
-            >
-              <img
-                width="20"
-                height="20"
-                src="https://img.icons8.com/color/48/delete-sign--v1.png"
-                alt="delete-sign--v1"
-              />
-            </button>
-          ) : null}
+        {onDismiss !== undefined ? (
+          <button
+            type="button"
+            className="border-0 bg-transparent"
+            onClick={onDismiss}
+          >
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/color/48/delete-sign--v1.png"
+              alt="delete-sign--v1"
+            />
+          </button>
+        ) : null}
       </div>
     </div>
   );
--- src/Containers/ModalWindow/ModalWindow.tsx
+++ src/Containers/ModalWindow/ModalWindow.tsx
@@ -3,25 +3,38 @@ import { useState } from 'react';
 import Alert from '../../Components/UI/Alert/Alert.tsx';
 
 const ModalWindow = () => {
-  const [showModal, setShowModal] = useState<boolean>(true);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [showAlert, setShowAlert] = useState<boolean>(true);
 
   const cancel = () => {
     setShowModal(!showModal);
   };
 
   const closeAlert = () => {
-    console.log("You clicked to the button close (X)!");
+    setShowAlert(false);
   };
 
   return (
     <div className="container mt-5">
+      <button
+        className="btn btn-outline-primary mb-4"
+        onClick={() => setShowModal(!showModal)}
+      >
+        Show modal
+      </button>
       <Modal show={showModal} onClose={cancel} title="Some kinda modal title">
         <p>This is modal content</p>
       </Modal>
-      <Alert type="warning" onDismiss={closeAlert}>
-        This is a warning type alert
-      </Alert>
-      <Alert type="success">This is a success type alert</Alert>
+      <div>
+        {showAlert ? (
+          <>
+            <Alert type="warning" onDismiss={closeAlert}>
+              This is a warning type alert
+            </Alert>
+          </>
+        ) : null}
+        <Alert type="success">This is a success type alert</Alert>
+      </div>
     </div>
   );
 };
